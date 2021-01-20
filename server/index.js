216cff6ad0c7c5099d1d807dcbfccf388f5aa2f7
shubhsharma23 <ss9555958@gmail.com>
2021-01-20 19:10:36 +0530

    fix package

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: '20mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);

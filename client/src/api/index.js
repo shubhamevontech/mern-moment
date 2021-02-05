c800a1bb79fcd69be50b43ddd7f67e2c4723ddcb
shubhsharma23 <ss9555958@gmail.com>
2021-02-05 10:53:55 +0530

    server changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://mern-moment.herokuapp.com/' });
 
 
 API.interceptors.request.use((req) => {
--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
   res.send('welcome to moment API');
 });
 
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
